71ba99fe47beeb6cb246f732c32c13be937b6ecf
Umesh <umeshghaskata2001@gmail.com>
2026-07-26 16:56:51 -0400

    CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "http://54.174.77.245:5173"
+    ],
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
